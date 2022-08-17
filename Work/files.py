ebf4d2b2b5c8954a1e8be648129ff22ea5d983c3
Yury Tikhonov <yura.tikhonov@gmail.com>
2022-08-17 08:27:35 +0300

    Read from files

--- Work/files.py
+++ Work/files.py
@@ -4,17 +4,26 @@ import os
 
 print(os.getcwd())
 
-f=open('Work/Data/portfolio.csv', 'rt')
-data = f.read()
+# f=open('Work/Data/portfolio.csv', 'rt')
+# data = f.read()
 
 
-with open('Data/portfolio.csv', 'rt') as f:
-    data = f.read()
+# with open('Work/Data/portfolio.csv', 'rt') as f:
+#     data = f.read()
 
-f1 = open('Data/portfolio2.csv','rt')
+f1 = open('Work/Data/portfolio2.csv','rt')
 
+data0 = f1.readline()
+data00 = f1.readlines(3)
 data1 = f1.read()
+data2 = f1.read() # it will be empty
 
 f1.close()
 
+print(data0)
+print()
+print(data00)
+print()
 print(data1)
+print()
+print(data2)
