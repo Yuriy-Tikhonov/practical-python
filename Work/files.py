0b4375ae4c54db11e34b6833275ad598cf953a54
Yury Tikhonov <yura.tikhonov@gmail.com>
2022-08-17 05:05:46 +0300

    files doesn't work

--- Work/files.py
+++ Work/files.py
@@ -1,1 +1,20 @@
 # https://dabeaz-course.github.io/practical-python/Notes/01_Introduction/06_Files.html
+
+import os
+
+print(os.getcwd())
+
+f=open('Data/portfolio.csv', 'rt')
+data = f.read()
+
+
+with open('Data/portfolio.csv', 'rt') as f:
+    data = f.read()
+
+f1 = open('Data/portfolio2.csv','rt')
+
+data1 = f1.read()
+
+f1.close()
+
+print(data1)
