ebb0aacf52c91192d84dc89668f2f2fd4e5531fa
Yury Tikhonov <yura.tikhonov@gmail.com>
2022-08-18 00:37:03 -0700

    Files

--- Work/files.py
+++ Work/files.py
@@ -4,17 +4,10 @@ import os
 
 print(os.getcwd())
 
-# f=open('Work/Data/portfolio.csv', 'rt')
-# data = f.read()
-
-
-# with open('Work/Data/portfolio.csv', 'rt') as f:
-#     data = f.read()
-
-f1 = open('Work/Data/portfolio2.csv','rt')
+f1 = open('Work/Data/portfolio.csv','rt')
 
 data0 = f1.readline()
-data00 = f1.readlines(3)
+data00 = f1.readlines(20) # 20 - the parameter is in bytes 
 data1 = f1.read()
 data2 = f1.read() # it will be empty
 
@@ -27,3 +20,30 @@ print()
 print(data1)
 print()
 print(data2)
+
+print('-------------------')
+
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    data_with = f.read()
+    print(data_with)
+    print()
+
+print('-------------------')
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    for line in f:
+        print(line)
+        # print('-=-=-=-=-=')
+
+print('-------------------')
+with open('Work/Data/portfolio.csv', 'rt') as fi:
+    with open('Work/Data/output', 'wt') as fw:
+        for line in fi:
+            fw.write(line)
+            print('-=-=-=-=-=-=-=-=-=', file=fw)
+
+import gzip
+
+with gzip.open('Work/Data/portfolio.csv.gz', 'rt') as gf:
+    for line in gf:
+        print(line)
+
