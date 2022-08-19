662acbf11f0039a7dbee65d449bffb4d18bc7b3a
Yury Tikhonov <yura.tikhonov@gmail.com>
2022-08-18 18:04:47 -0700

    functions

---
+++ Work/functions.py
@@ -0,0 +1,20 @@
+# functions
+
+def sum(a, b):
+    return a+b
+
+a = sum(1,2)
+print(a)
+
+import math
+
+print(math.cos(30))
+
+print(math.sqrt(2))
+
+import urllib.request
+
+urlData = urllib.request.urlopen('https://google.com')
+a = urlData.read()
+print(a)
+
