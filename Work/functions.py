f34b449414c2c0c51759483881111dfc3dfc408f
Yury Tikhonov <yura.tikhonov@gmail.com>
2022-08-19 22:52:29 -0700

    functions and exceptions

--- Work/functions.py
+++ Work/functions.py
@@ -7,6 +7,7 @@ a = sum(1,2)
 print(a)
 
 import math
+from signal import raise_signal
 
 print(math.cos(30))
 
@@ -17,4 +18,27 @@ import urllib.request
 urlData = urllib.request.urlopen('https://google.com')
 a = urlData.read()
 print(a)
+
+try:
+    int('N/A')
+    int('123')
+except (ValueError, RuntimeError) as exc: # several types
+    print('It was Value Error')
+    print(exc)
+#    raise RuntimeError('Help')  # raise without processing
+else:                           # no error processing block
+    print('No error')
+finally:
+    print('finally')
+
+
+# try:
+#     raise RuntimeError22('Help')  # raise without processing
+# except RuntimeError22 as exc: # several types
+#     print('It was RuntimeError22 Error')
+#     print(exc)
+# else:                           # no error processing block
+#     print('No error')
+# finally:
+#     print('finally')
 
